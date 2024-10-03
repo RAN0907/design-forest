a2d3b5fc3c460331e549afaf222bfb468a7dc422
RAN0907 <misscoco6237@naver.com>
2024-10-03 13:01:53 +0900

    feat: apply design-forest project

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+import { menu } from "./menu.js";
+// import { port } from "./port.js";
+import { link } from "./link.js";
+import { skill } from "./skill.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener("load", function(){
+    menu();
+    // port();
+    link();
+    skill();
+    smooth();
+});
